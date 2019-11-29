47e3cecaf30e9285f0b5ff085b8f3abf8bc67ada
HUSTyankang <1227752726@qq.com>
2019-11-29 14:53:42 +0800

    proxy

---
+++ spring-learn-proxy-01/src/main/java/com/spring/learn/proxy/IProducer.java
@@ -0,0 +1,19 @@
+package com.spring.learn.proxy;
+
+/**
+ * 对生产厂家要求的接口
+ */
+public interface IProducer {
+    /**
+     * 销售
+     * @param money
+     */
+    public void saleProduct(float money);
+
+    /**
+     * 售后
+     * @param money
+     */
+    public void afterService(float money);
+
+}
